0065b49362dad2a9264c8dc16cdefbc10cb26415
csf2025 <csf@gmx.net>
2025-03-23 07:50:23 -0400

    update

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,4 +6,19 @@ def emotion_detector(text_to_analyze):
     input_json = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, json=input_json, headers=headers )
-    return response.text
+    response = json.loads(response.text)
+
+    emotions = response.get('emotionPredictions')[0].get('emotion')
+    anger_score = emotions.get('anger')
+    disgust_score = emotions.get('disgust')
+    fear_score = emotions.get('fear')
+    joy_score = emotions.get('joy')
+    sadness_score = emotions.get('sadness')
+
+    formatted_resonse = {'anger': anger_score,
+    'disgust': disgust_score,
+    'fear': fear_score,
+    'joy': joy_score,
+    'sadness': sadness_score}
+    
+    return formatted_resonse
