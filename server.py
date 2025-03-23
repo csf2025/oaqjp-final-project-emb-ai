090d12e79831070c6795491888a09bc65e920e5e
csf2025 <csf@gmx.net>
2025-03-23 15:10:34 -0400

    task 7

--- server.py
+++ server.py
@@ -9,22 +9,24 @@ def display_init_page():
 
 @app.route("/emotionDetector")
 def detect_emotions():
-
     text_to_analyze = request.args.get('textToAnalyze')
 
-    response = emotion_detector(text_to_analyze)
+    if not text_to_analyze:
+        return "Invalid text! Please try again!",400
 
-    anger_score = response.get('anger')
-    disgust_score = response.get('disgust')
-    fear_score = response.get('fear')
-    joy_score = response.get('joy')
-    sadness_score = response.get('sadness')
-    dominant_emotion = response.get('dominant_emotion')
+    else:
+        response = emotion_detector(text_to_analyze)
+        anger_score = response.get('anger')
+        disgust_score = response.get('disgust')
+        fear_score = response.get('fear')
+        joy_score = response.get('joy')
+        sadness_score = response.get('sadness')
+        dominant_emotion = response.get('dominant_emotion')
 
-    formatted_response = f"""For the given statement, the system response is 'anger': {anger_score},
-    'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and
-    'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}."""
+        formatted_response = f"""For the given statement, the system response is 'anger': {anger_score},
+        'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and
+        'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}."""
+        
+        return formatted_response
 
-    return formatted_response
-
-app.run(host='127.0.0.1', port = 5000, debug = True)
+app.run(host='127.0.0.1', port = 5001, debug = True)
